22a688edf617e96bdf635978bcd0578c49aecfc0
Justin <justindy1112@yahoo.com>
2016-02-11 22:33:05 +0800

    property production progress bars

--- core/src/com/epicnoobz/tycoons/objects/Property.java
+++ core/src/com/epicnoobz/tycoons/objects/Property.java
@@ -1,16 +1,27 @@
 package com.epicnoobz.tycoons.objects;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ArrayMap;
+import com.badlogic.gdx.utils.TimeUtils;
+import com.epicnoobz.tycoons.Tycoons;
+import com.epicnoobz.tycoons.screens.AbstractScreen;
 
 public class Property {
 	private String name;
 	private int level;
+	private boolean shouldStartProduction = true;
 	private float timeToCollect;
+	private double timeElapsed;
+	private double timeRemaining;
+	private String timeRemainingString;
 	private Array<ArrayMap<Resource, Integer>> requirements;
 	private Array<ArrayMap<Resource, Integer>> products;
 	private TextureRegion texture;
+	private GameProgressBar productionProgress;
+	private long startTime;
 
 	public Property(String name, int level, float timeToCollect,
 			TextureRegion texture) {
@@ -19,7 +30,103 @@ public class Property {
 		this.timeToCollect = timeToCollect;
 		this.texture = texture;
 	}
+	
+	public void registerStartTime(){
+		startTime = TimeUtils.millis();
+	}
+	
+	public void setTimeElapsed(){
+		if(timeElapsed != timeToCollect){
+			timeElapsed = convertToSeconds("milliseconds",TimeUtils.timeSinceMillis(startTime));
+		}
+		
+		timeRemaining = timeToCollect - timeElapsed;
+		if(timeRemaining < 0){
+			timeRemaining = 0;
+		}
+		setTimeRemainingString(timeRemaining);
+	}
+	
+	private void setTimeRemainingString(double timeRemaining){
+		int days = 0;
+		int hours = 0;
+		int minutes = 0;
+		int seconds = 0;
+		if(timeRemaining < 60){
+			seconds = (int)timeRemaining;
+		}
+		if(timeRemaining >= 60){
+			seconds = (int)(timeRemaining % 60);
+			minutes = (int)(timeRemaining / 60);
+		}
+		else if (minutes >= 60){
+			minutes = (int)(minutes % 60);
+			hours = (int)(minutes / 60);
+		}
+		else if (hours >= 24){
+			hours = (int)(hours % 24);
+			days = (int)(hours / 24);
+		}
+		timeRemainingString = "";
+		if(days > 0){
+			timeRemainingString += days + "D ";
+		}
+		if(hours > 0){
+			timeRemainingString += hours + "H ";
+		}
+		if(minutes > 0){
+			timeRemainingString += minutes + "M ";
+		}
+		if(seconds > 0){
+			timeRemainingString += seconds + "S ";
+		}
+		if(timeRemaining == 0){
+			timeRemainingString = "COLLECT";
+		}
+	}
+	
+	public double convertToSeconds(String type, double value){
+		if(type.equals("minutes")){
+			return value * 60;
+		}
+		else if(type.equals("milliseconds")){
+			return value / 1000;
+		}
+		return value;
+	}
+	
+	public double getTimeElapsed() {
+		return timeElapsed;
+	}
+	
+	public boolean getShouldStartProduction() {
+		return shouldStartProduction;
+	}
 
+	public void setShouldStartProduction(boolean shouldStartProduction) {
+		this.shouldStartProduction = shouldStartProduction;
+	}
+	
+	public void initProductionProgressBar(TextureRegion barTimeLeftEmpty, TextureRegion barTimeLeftFull){
+		productionProgress = new GameProgressBar(0,timeToCollect,1,barTimeLeftEmpty,barTimeLeftFull,false);
+	}
+	
+	public GameProgressBar getProductionProgress() {
+		return productionProgress;
+	}
+
+	public double getTimeRemaining() {
+		return timeRemaining;
+	}
+	
+	public String getTimeRemainingString() {
+		return timeRemainingString;
+	}
+
+	public void setTimeRemaining(float timeRemaining) {
+		this.timeRemaining = timeRemaining;
+	}
+	
 	public String getName() {
 		return name;
 	}
--- core/src/com/epicnoobz/tycoons/screens/PropertiesScreen.java
+++ core/src/com/epicnoobz/tycoons/screens/PropertiesScreen.java
@@ -1,5 +1,11 @@
 package com.epicnoobz.tycoons.screens;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
@@ -12,15 +18,16 @@ import com.badlogic.gdx.utils.Array;
 import com.epicnoobz.tycoons.Tycoons;
 import com.epicnoobz.tycoons.objects.GameProgressBar;
 import com.epicnoobz.tycoons.objects.Property;
+import com.esotericsoftware.minlog.Log;
 
 public class PropertiesScreen extends GameScreen {
 
-	Table properties;
-	GameProgressBar resourceCollect;
-	Group requires;
-	Group produces;
-	Array<GameProgressBar> timesToCollect;
-	Image sellProperty;
+	Table propertiesTable;
+	GameProgressBar resourceCollectProgressBar;
+	Group requiresGroup;
+	Group producesGroup;
+	Image sellPropertyImage;
+	Array<Property> properties;
 
 	public PropertiesScreen(Tycoons game) {
 		super(game, Tab.PROPERTIES);
@@ -31,6 +38,7 @@ public class PropertiesScreen extends GameScreen {
 		super.initActors();
 		initImageLabels();
 		initProperties();
+		initPropertiesTable();
 		initDivider();
 		initResourceCollect();
 		initRequires();
@@ -42,72 +50,59 @@ public class PropertiesScreen extends GameScreen {
 		showMoneyLabel(50, VIEWPORT_HEIGHT - 150, true);
 		showPropertiesLabel(730, VIEWPORT_HEIGHT - 150, false);
 	}
-
-	private void initProperties() {
-		properties = new Table();
-		properties.setSize(944, 1554);
-		properties.top();
-		ScrollPane scrollPane = new ScrollPane(properties);
-		scrollPane.setBounds(VIEWPORT_WIDTH / 2 - 472,
-				VIEWPORT_HEIGHT / 2 - 246, 944, 1036);
-		Array<Property> propertyList = game.state.getProperties();
-		Array<GameProgressBar> propertyProgressBars = new Array<GameProgressBar>(
-				propertyList.size);
-		for (Property property : propertyList) {
-			propertyProgressBars.add(new GameProgressBar(0, 60, property
-					.getTimeToCollect(),
-					atlas.findRegion("Bar_TimeLeft_Empty"), atlas
-							.findRegion("Bar_TimeLeft_Full"), false));
+	
+	private void initProperties(){
+		properties = game.state.getProperties();
+		properties.add(new Property("Coal Mine",1,100,atlas.findRegion("Building_Coal_Mine")));
+		properties.add(new Property("Cattle Farm",1,10,atlas.findRegion("Building_Cattle_Farm")));
+		properties.add(new Property("Bakery",1,1000,atlas.findRegion("Building_Bakery")));
+		properties.add(new Property("Bar",1,500,atlas.findRegion("Building_Bar")));
+		properties.add(new Property("Fruit Orchard",1,300,atlas.findRegion("Building_Fruit_Orchard")));
+		properties.add(new Property("Green House",1,400,atlas.findRegion("Building_Green_House")));
+		properties.add(new Property("Metal Ore Mine",1,200,atlas.findRegion("Building_Metal_Ore_Mine")));
+		for (Property property : properties) {
+			property.initProductionProgressBar(new TextureRegion(atlas.findRegion("Bar_TimeLeft_Empty")), new TextureRegion(atlas.findRegion("Bar_TimeLeft_Full")));
+			property.registerStartTime();
 		}
+	}
 
-		for (int i = 0; i < propertyList.size; i++) {
-
-		}
-
-		Property property = new Property("Coal Mine", 1, 10,
-				atlas.findRegion("Building_temp"));
-		GameProgressBar testBar = new GameProgressBar(0, 60, 1,
-				atlas.findRegion("Bar_TimeLeft_Empty"),
-				atlas.findRegion("Bar_TimeLeft_Full"), false);
-		testBar.setValue(30);
+	private void initPropertiesTable() {
+		propertiesTable = new Table();
+		ScrollPane scrollPane = new ScrollPane(propertiesTable);
 		scrollPane.setBounds(VIEWPORT_WIDTH / 2 - 472,
 				VIEWPORT_HEIGHT / 2 - 246, 944, 1036);
 		// size based on building images + pads
-		properties.setSize(944, 1554);
-		properties.top();
-		// Array<Property> list = game.state.getProperties();
-		properties.add(new Image(property.getTexture())).pad(20, 1, 20, 1);
-		properties.add().width(68);
-		properties.add(new Image(property.getTexture())).pad(20, 1, 20, 1);
-		properties.row();
-		properties.add(testBar).pad(0, 0, 20, 0);
-		testBar.setValue(50);
-		// properties.add(new
-		// Image(atlas.findRegion("Bar_TimeLeft_Full"))).pad(0,
-		// 0, 20, 0);
-		properties.add().width(68);
-		properties.add(new Image(atlas.findRegion("Bar_TimeLeft_Full")))
-				.padBottom(20);
-		properties.row();
-		properties.add(new Image(property.getTexture())).pad(20, 1, 20, 1);
-		properties.add().width(68);
-		properties.add(new Image(property.getTexture())).pad(20, 1, 20, 1);
-		properties.row();
-		properties.add(new Image(atlas.findRegion("Bar_TimeLeft_Full"))).pad(0,
-				0, 20, 0);
-		properties.add().width(68);
-		properties.add(new Image(atlas.findRegion("Bar_TimeLeft_Full")))
-				.padBottom(20);
-		properties.row();
-		properties.add(new Image(property.getTexture())).pad(20, 1, 20, 1);
-		properties.add().width(68);
-		properties.add(new Image(property.getTexture())).pad(20, 1, 20, 1);
-		properties.row();
-		properties.add(new Image(atlas.findRegion("Bar_TimeLeft_Full"))).pad(0,
-				0, 20, 0);
-		properties.add().width(68);
-		properties.add(new Image(atlas.findRegion("Bar_TimeLeft_Full")))
-				.padBottom(20);
+		propertiesTable.setSize(944, 1554);
+		propertiesTable.top();
+		
+		int property_ctr = 0;
+		GameProgressBar leftPropertyProgressBar = new GameProgressBar();
+		for (Property property : properties) {
+			//add property at the left side
+			if(property_ctr % 2 == 0){
+				if(property_ctr != 0){
+					propertiesTable.row();
+				}
+				propertiesTable.add(new Image(property.getTexture())).pad(20, 1, 20, 1);
+				propertiesTable.add().width(68);
+				leftPropertyProgressBar = property.getProductionProgress();
+			}
+			//add property at the right side and add the progress bars for left and right side
+			else{
+				propertiesTable.add(new Image(property.getTexture())).pad(20, 1, 20, 1);
+				propertiesTable.row();
+				propertiesTable.add(leftPropertyProgressBar).pad(0,
+						0, 20, 0);
+				propertiesTable.add().width(68);
+				propertiesTable.add(property.getProductionProgress()).padBottom(20);
+			}
+			property_ctr++;
+		}
+		if(property_ctr % 2 == 1){
+			propertiesTable.row();
+			propertiesTable.add(leftPropertyProgressBar).pad(0,
+					0, 20, 0);
+		}
 		stage.addActor(scrollPane);
 	}
 
@@ -120,50 +115,50 @@ public class PropertiesScreen extends GameScreen {
 	}
 
 	private void initResourceCollect() {
-		resourceCollect = new GameProgressBar(1, 10, 1,
+		resourceCollectProgressBar = new GameProgressBar(1, 10, 1,
 				atlas.findRegion("Bar_ResourceCollected_BG"),
 				atlas.findRegion("Bar_ResourceCollected_Full"), false);
-		resourceCollect.addHighlight(atlas
+		resourceCollectProgressBar.addHighlight(atlas
 				.findRegion("Bar_ResourceCollected_Highlight"));
-		resourceCollect.setPosition(
-				VIEWPORT_WIDTH / 2 - resourceCollect.getWidth() / 2,
-				VIEWPORT_HEIGHT / 2 - resourceCollect.getHeight() - 302);
-		resourceCollect.setTouchable(Touchable.disabled);
-		stage.addActor(resourceCollect);
+		resourceCollectProgressBar.setPosition(
+				VIEWPORT_WIDTH / 2 - resourceCollectProgressBar.getWidth() / 2,
+				VIEWPORT_HEIGHT / 2 - resourceCollectProgressBar.getHeight() - 302);
+		resourceCollectProgressBar.setTouchable(Touchable.disabled);
+		stage.addActor(resourceCollectProgressBar);
 	}
 
 	private void initRequires() {
-		requires = new Group();
+		requiresGroup = new Group();
 		Image bg = new Image(atlas.findRegion("BG_RequiredResource"));
-		requires.setBounds(50, resourceCollect.getY() - bg.getHeight() - 20,
+		requiresGroup.setBounds(50, resourceCollectProgressBar.getY() - bg.getHeight() - 20,
 				bg.getWidth(), bg.getHeight());
-		requires.addActor(bg);
-		requires.setTouchable(Touchable.disabled);
-		stage.addActor(requires);
+		requiresGroup.addActor(bg);
+		requiresGroup.setTouchable(Touchable.disabled);
+		stage.addActor(requiresGroup);
 	}
 
 	private void initProduces() {
-		produces = new Group();
+		producesGroup = new Group();
 		Image bg = new Image(atlas.findRegion("BG_ProducedResource"));
-		produces.setBounds(requires.getRight() + 10, resourceCollect.getY()
+		producesGroup.setBounds(requiresGroup.getRight() + 10, resourceCollectProgressBar.getY()
 				- bg.getHeight() - 20, bg.getWidth(), bg.getHeight());
-		produces.addActor(bg);
-		produces.setTouchable(Touchable.disabled);
-		stage.addActor(produces);
+		producesGroup.addActor(bg);
+		producesGroup.setTouchable(Touchable.disabled);
+		stage.addActor(producesGroup);
 	}
 
 	private void initSellProperty() {
-		final TextureRegionDrawable sellPropertyClicked = new TextureRegionDrawable(
+		final TextureRegionDrawable sellPropertyImageClicked = new TextureRegionDrawable(
 				atlas.findRegion("Button_SellProperty_Clicked"));
-		final TextureRegionDrawable sellPropertyNeutral = new TextureRegionDrawable(
+		final TextureRegionDrawable sellPropertyImageNeutral = new TextureRegionDrawable(
 				atlas.findRegion("Button_SellProperty_Neutral"));
-		sellProperty = new Image(sellPropertyNeutral);
-		sellProperty.addListener(new ClickListener() {
+		sellPropertyImage = new Image(sellPropertyImageNeutral);
+		sellPropertyImage.addListener(new ClickListener() {
 
 			@Override
 			public boolean touchDown(InputEvent event, float x, float y,
 					int pointer, int button) {
-				sellProperty.setDrawable(sellPropertyClicked);
+				sellPropertyImage.setDrawable(sellPropertyImageClicked);
 				return super.touchDown(event, x, y, pointer, button);
 			}
 
@@ -171,22 +166,32 @@ public class PropertiesScreen extends GameScreen {
 			public void touchUp(InputEvent event, float x, float y,
 					int pointer, int button) {
 				super.touchUp(event, x, y, pointer, button);
-				sellProperty.setDrawable(sellPropertyNeutral);
+				sellPropertyImage.setDrawable(sellPropertyImageNeutral);
 			}
 
 			@Override
 			public void clicked(InputEvent event, float x, float y) {
-				// TODO sell property
+				//TODO sell property
 			}
 
 		});
-		sellProperty.setPosition(sound.getX() - 10, sound.getTop() + 10);
-		stage.addActor(sellProperty);
+		sellPropertyImage.setPosition(sound.getX() - 10, sound.getTop() + 10);
+		stage.addActor(sellPropertyImage);
 	}
 
 	@Override
 	public void render(float delta) {
 		super.render(delta);
+		for (Property property : properties) {
+			GameProgressBar productionProgress = property.getProductionProgress();
+			if(property.getShouldStartProduction()){
+				property.setTimeElapsed();
+				productionProgress.setValue((float)Math.ceil(property.getTimeElapsed()));
+				productionProgress.setFont(new BitmapFont(Gdx.files.internal("font/tycoons.fnt")));
+				productionProgress.setText(property.getTimeRemainingString());
+			}
+		}
+		
 	}
 
 }
